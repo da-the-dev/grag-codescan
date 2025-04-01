cc409cd3eaa5a9691992d6b6d7dd75f6727ee1c4
PodYapolskiy <podyapolskiyaa@gmail.com>
2025-04-01 12:01:58 +0000

    fix: make sidebar textboxes interactible and constrain with max_lines of 1

--- src/ui/sidebar.py
+++ src/ui/sidebar.py
@@ -19,19 +19,27 @@ def sidebar():
         repo = gr.Textbox(
             value=settings.sidebar.REPO or "",
             label="Repo name",
+            max_lines=1,
+            interactive=True,
         )
         owner = gr.Textbox(
             value=settings.sidebar.OWNER or "",
             label="Owner",
+            max_lines=1,
+            interactive=True,
         )
         branch = gr.Textbox(
             value=settings.sidebar.BRANCH or "",
             label="Branch name",
+            max_lines=1,
+            interactive=True,
         )
         token = gr.Textbox(
             value=settings.sidebar.TOKEN or "",
             label="Token",
             type="password",
+            max_lines=1,
+            interactive=True,
         )
 
         run = gr.Button("Start analysis")
